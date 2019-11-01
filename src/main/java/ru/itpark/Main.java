a88738688bd5dbce944d408b1f2c441d0dabf8ed
Almir <almir.almiev@tatar.ru>
2019-11-01 18:10:47 +0300

    fixes #1

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -3,22 +3,4 @@ package ru.itpark;
 import java.sql.*;
 
 public class Main {
-    /*public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:ulmart.db")) {
-            try (Statement statement = connection.createStatement()) {
-                try (final ResultSet resultSet = statement.executeQuery("SELECT id, name, salary FROM managers")){
-                    while (resultSet.next()) {
-                        System.out.println(resultSet.getInt("id"));
-                        System.out.println(resultSet.getString("name"));
-                        System.out.println(resultSet.getInt("salary"));
-                    }
-                    resultSet.close();
-                    statement.close();
-                    connection.close();
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
