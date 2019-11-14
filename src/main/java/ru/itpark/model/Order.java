a3425de17c1a52746c13b7f666aeb8434e14e254
Almir <almir.almiev@tatar.ru>
2019-11-14 14:29:15 +0300

    fixes #17

--- src/main/java/ru/itpark/model/Order.java
+++ src/main/java/ru/itpark/model/Order.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+    private List<Product> products;
     private int count;
     private String name;
     private int price;
